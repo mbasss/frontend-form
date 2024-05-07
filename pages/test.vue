63d8ef85000210e122a933b41a0ff3188e488f0d
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-07 08:44:27 +0700

    Layout Auth

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <h1>Test</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'TestPage',
+  layout:  'auth'
+}
+</script>
