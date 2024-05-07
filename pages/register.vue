9ea9157721cf918584f04d27e432cb43d9595ee7
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-07 09:01:07 +0700

    Page Register

---
+++ pages/register.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-row>
+    <v-col cols="10" offset="1" md="4" offset-md="4">
+      <v-card>
+        <v-toolbar color="primary" dark>REGISTER</v-toolbar>
+
+        <v-card-text>
+          <v-form>
+            <v-text-field
+             label= "Name" 
+             required
+            />
+            <v-text-field
+             label= "Email" 
+             required
+            />
+            <v-text-field
+             label= "Password" 
+             required
+            />
+            <v-text-field
+             label= "Confirm Password" 
+             required
+            />
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary">Register</v-btn>
+        </v-card-actions>
+      </v-card>
+      <div class="d-flex align-baseline">
+        <p>
+          Sudah punya akun? 
+          <v-btn 
+          text
+          plain
+          to="/login"
+          color="primary"
+          :ripple="false"
+          class="pl-1"
+          >Login</v-btn>
+        </p>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'TestPage',
+  layout:  'auth'
+}
+</script>
