b2e9ef2308d325b9b44c61d4c97beb5c5682fd7f
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-07 09:22:11 +0700

    Rules Form Register

--- pages/register.vue
+++ pages/register.vue
@@ -7,19 +7,27 @@
         <v-card-text>
           <v-form>
             <v-text-field
-             label= "Name" 
+             label= "Name"
+             :rules="rules.fullname"
+             v-model="form.fullname"
              required
             />
             <v-text-field
              label= "Email" 
+             :rules="rules.email"
+             v-model="form.email"
              required
             />
             <v-text-field
              label= "Password" 
+             :rules="rules.password"
+             v-model="form.password"
              required
             />
             <v-text-field
-             label= "Confirm Password" 
+             label= "Confirm Password"
+             :rules="rules.confirmPassword"
+             v-model="form.confirmPassword"
              required
             />
           </v-form>
@@ -49,7 +57,34 @@
 
 <script>
 export default {
-  name: 'TestPage',
-  layout:  'auth'
+  layout:  'auth',
+
+  data () {
+    return {
+      form : {
+        fullname: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      },
+      rules: {
+        fullname: [
+          val => !!val || 'Fullname is required'
+        ],
+        email: [
+          val => !!val || 'Email is required',
+          val => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val) || 'Email must be valid'
+        ],
+        password: [
+          val => !!val || 'Password is required',
+          val => val.length >= 6 || 'Password must be at least 6 characters'
+        ],
+        confirmPassword: [
+          val => !!val || 'Confirm Password is required',
+          val => val === this.form.password || 'Password does not match'
+        ]
+      }
+    }
+  }
 }
 </script>
