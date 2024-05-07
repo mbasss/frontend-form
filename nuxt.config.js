eb6c043a4b75788efda0a9c257d124cf4f885fc2
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-07 10:43:20 +0700

    ubah port

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   ssr: false,
+  server: {
+    port : 8000, //default: 3000
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - MbaSSS Form',
