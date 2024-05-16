9834462580856d274c4a19b8597535e746195168
Indra <indra.kurniawan@kasn.go.id>
2024-05-16 16:00:49 +0700

    sesuaikan package seperti tutorial

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - MbaSSS Form',
-    title: 'form-nuxt-frontend',
+    title: 'form-frontend',
     htmlAttrs: {
       lang: 'en'
     },
