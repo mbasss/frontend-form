7875dcbe2dcc74b2f4a614a0450b760f8a2233a9
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-17 23:16:35 +0700

    Middleware Unauthenticated

--- pages/login.vue
+++ pages/login.vue
@@ -53,6 +53,7 @@
 <script>
 import {mapState} from 'vuex';
 export default {
+  middleware: ['unauthenticated'],
   layout:  'auth',
   data() {
     return {
--- pages/register.vue
+++ pages/register.vue
@@ -64,8 +64,8 @@
 
 <script>
 export default {
+  middleware: ['unauthenticated'],
   layout:  'auth',
-
   data () {
     return {
       isLoading: false,
