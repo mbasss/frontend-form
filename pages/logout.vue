07b76e1e446b90d54a2e16b14b8924533b30848a
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-21 14:00:49 +0700

    2 cara logout

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,9 +1,16 @@
+<template></template>
 <script>
+import * as Cookies from "js-cookie";
 export default {
   middleware: ['authenticated'],
   mounted () {
+    // Cara 1
+    // Cookies.remove('mbasss-form-auth');
+    // window.location.href = '/login';
+
+    // Cara 2
     this.$store.commit('auth/logout')
-    this.$router.push({name: 'index___' + this.$i18n.locale})
+    this.$router.push({name: 'login___' + this.$i18n.locale})
   }
 }
 </script>
