ab68e29b5d8a5e0aadd43148001e4cf821d88c5d
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-28 08:13:01 +0700

    Handled Error Refresh Token

--- plugins/axiosInterceptors.js
+++ plugins/axiosInterceptors.js
@@ -4,4 +4,27 @@ export default function ({ $axios, redirect, store }) {
       config.headers['Authorization'] = `Bearer ${store.state.auth.accessToken}`
     }
   })
+
+  $axios.onResponseError(async error => {
+    try {
+      if(error.response.status === 401) {
+        let refreshToken = store.state.auth.refreshToken
+
+        const response = await $axios.$post('/refresh-token', {'refreshToken': refreshToken})
+
+        // menyimpan token baru ke store/auth
+        store.commit('auth/setAccessToken', response.data.accessToken)
+        store.commit('auth/setRefreshToken', response.data.refreshToken)
+
+        // jalankan ulang permintaan terakhir
+        let originalRequest = error.config;
+        originalRequest.headers['Authorization'] = `Bearer ${response.accessToken}`
+
+        return $axios(originalRequest)
+      }
+    } catch (error) {
+      console.log(error);
+      
+    }
+  })
 }
