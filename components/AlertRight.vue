f4f7eb3b674104ea6ddec6cb4c16c3f23bfaea7a
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-05-28 08:42:26 +0700

    Kolom Message & Kolom Button Close

--- components/AlertRight.vue
+++ components/AlertRight.vue
@@ -6,7 +6,21 @@
     type="error"
     class="v-bg-white"
     >
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas alias consequuntur quaerat ipsum beatae. Dolore et consequatur ipsa, minima minus suscipit rem assumenda illo in inventore eveniet fugit quidem aliquam mollitia, nulla, reiciendis fugiat saepe? Similique aperiam pariatur laborum debitis atque quia! Adipisci consectetur delectus totam fugiat. Harum eaque totam neque labore in facere fugit ut veritatis minus dolores ipsa aliquid laudantium aspernatur eius autem enim temporibus possimus porro, eos modi magni repellat commodi. Facere non, dolore quam reiciendis soluta ut quia sint? Magnam quod commodi quo distinctio, beatae ipsam voluptatum quidem exercitationem tempore modi eius quam corrupti laborum harum?      
+    <v-row>
+      <v-col class="grow">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas alias consequuntur quaerat ipsum beatae. Dolore et consequatur ipsa, minima minus suscipit rem assumenda illo in inventore eveniet fugit quidem aliquam mollitia, nulla, reiciendis fugiat saepe? Similique aperiam pariatur laborum debitis atque quia! Adipisci consectetur delectus totam fugiat. Harum eaque totam neque labore in facere fugit ut veritatis minus dolores ipsa aliquid laudantium aspernatur eius autem enim temporibus possimus porro, eos modi magni repellat commodi. Facere non, dolore quam reiciendis soluta ut quia sint? Magnam quod commodi quo distinctio, beatae ipsam voluptatum quidem exercitationem tempore modi eius quam corrupti laborum harum?
+      </v-col>
+      <v-col class="shrink">
+        <v-btn
+        small
+        color="error"
+        icon
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+          
     </v-alert>
   </div>
 </template>
