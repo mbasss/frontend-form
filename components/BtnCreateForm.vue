2d0b9797785f2caea6b510a3f86698a5f383099f
Indra Kurniawan <indra.kurniawan@kasn.go.id>
2024-06-01 11:29:12 +0700

    Button Bikin Form

---
+++ components/BtnCreateForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn
+  :loading="isLoading"
+  color="primary"
+  @click="createNewForm"
+  >
+    New Form
+    <v-icon>mdi-plus</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoading: false
+    }
+  },
+  methods: {
+    createNewForm() {
+      this.isLoading = true
+      // this.$router.push('/form/create')
+    }
+  }
+
+}
+</script>
