60603307480572dd265c110fbf134323e9f6c575
Indra <indra.kurniawan@kasn.go.id>
2024-06-03 15:31:30 +0700

    Toolbar Question dan Response

---
+++ components/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar 
+  flat
+  height="40">
+    <v-spacer></v-spacer>
+    <v-btn  
+    plain 
+    :ripple="false"
+    to="/">
+        Question
+    </v-btn>
+
+    <v-btn text 
+    plain 
+    :ripple="false"
+    to="/">
+        Response
+    </v-btn>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
