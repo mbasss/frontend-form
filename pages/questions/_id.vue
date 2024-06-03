0a9f1aa1c3ee1a720c6e9dc86e5168f02da47d88
Indra <indra.kurniawan@kasn.go.id>
2024-06-03 15:46:16 +0700

    Page Questions

---
+++ pages/questions/_id.vue
@@ -0,0 +1,34 @@
+
+import Toolbar from '../../components/Toolbar.vue';
+<template>
+  <div>
+    <v-container
+    fluid
+    class="pa-0"
+    >
+    <Toolbar />
+  </v-container>
+
+  <v-container>
+    <v-row>
+      <v-col md="8" offset-md="2" sm="10" offset-sm="1 ">
+        <v-card>
+          <v-card-text>
+            Test
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col md="8" offset-md="2" sm="10" offset-sm="1 ">
+        <v-card>
+          <v-card-text>
+            Test
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</div>
+</template>
