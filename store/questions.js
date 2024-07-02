e48f648396d7bceaf22a6394245be4971f2b2611
Indra <indra.kurniawan@kasn.go.id>
2024-07-02 08:39:40 +0700

    72 Update State Type

--- store/questions.js
+++ store/questions.js
@@ -10,6 +10,10 @@ export const state = () => ({
 export const mutations = {
   set(state, questions) {
     state.questions = questions
+  },
+  update(state, payload) {
+    const index = state.questions.findIndex(q => q.id === payload.questionId)
+    state.questions[index].type = payload.form.type
   }
 }
 
@@ -24,6 +28,7 @@ export const actions = {
     const response = await this.$axios.$put(`/forms/${payload.formId}/questions/${payload.questionId}`, payload.form, config)
     if(!response) {return false}
 
+    commit('update', payload)
     return response
   }
 }
